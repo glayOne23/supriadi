f43c75f6206a6330cb298a13aac51eb8c5bf8171
Muhammad Hammam Islami, S.Kom <mhi595@ums.ac.id>
2022-08-27 21:47:46 +0700

    [fixing] csrf origin in django 4.0 p.2

--- web/main_frontend/settings_docker.py
+++ web/main_frontend/settings_docker.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY'),
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
