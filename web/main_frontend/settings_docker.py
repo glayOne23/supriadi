e8d3cf674a1cc7d6de84e66e93378dbc32f166f9
Muhammad Hammam Islami, S.Kom <mhi595@ums.ac.id>
2022-08-27 20:44:47 +0700

    [update] dockerfile and docker-compose p.2

--- web/main_frontend/settings_docker.py
+++ web/main_frontend/settings_docker.py
@@ -75,16 +75,10 @@ WSGI_APPLICATION = 'main_frontend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-	'default':{
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DEFAULT_HOST', 'localhost'),
-		'PORT': os.getenv('3306'),
-		'OPTIONS': {
-		}
-	},
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
